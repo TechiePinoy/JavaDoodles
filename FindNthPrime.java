4449b1fdf4ee1811dd610d8023161a1e77584611
unknown <magbanua.c.ariel@gmail.com>
2021-09-20 17:56:42 +0800

    Corrected the java class docs and indentation.

--- FindNthPrime.java
+++ FindNthPrime.java
@@ -1,63 +1,57 @@
 import java.util.Scanner;
 
-/* This a rudimentary program that 
- * looks for the nth prime number
- * with user's choice of n > 0. 
+/**
+ * This a rudimentary program that looks for the nth prime number with user's
+ * choice of n > 0.
  * 
- * Current version works reasonably 
- * fast for up to n = ~100,000 on my PC.
- * 
- * @author: Facebook.com/TechiePinoy
- * 
+ * @TODO:
  * Future versions (if ever) should include:
  * 1. Exception handling (for non-natural number inputs)
  * 2. A better algorithm to minimize bit complexity.
+ *
+ * @author: TechiePinoy
+ * facebook.com/TechiePinoy
  */
-
 public class FindNthPrime {
-
 	public static void main(String[] args) {
-	
 		System.out.println("This prog looks for nth prime.");
 		System.out.println("Input n (n > 0):");
-		
+
 		Scanner z = new Scanner(System.in);
-		
+
 		int n = z.nextInt(); // user enters desired n
 
 		z.close();
-	
-		if ( n < 3 ) {
-			if (n == 1 ) {
+
+		if (n < 3) {
+			if (n == 1) {
 				System.out.println();
-				System.out.println ("Prime no. 1 = 2");
-				System.out.println();	
+				System.out.println("Prime no. 1 = 2");
+				System.out.println();
 			} else {
 				System.out.println();
-				System.out.println ("Prime no. 2 = 3");
-				System.out.println();	
+				System.out.println("Prime no. 2 = 3");
+				System.out.println();
 			}
-			
-		} else { // for n > 1
-			
+		} else {
+			// for n > 1
 			int forTest = 3;
 			int pCount = 2;
 			int mod;
 			int div = 3;
-			
+
 			while (pCount < n) {
-				
-				forTest = forTest + 2; //tests next number
-				
+				forTest = forTest + 2; // tests next number
+
 				while (div < forTest || div == 3) {
-					
+
 					mod = forTest % div; // tests divisibility, preps next divisor
-					
+
 					if (mod == 0) { // if divisible
 						div = 3; // resets div for next candidate
 						break; // ends test for current candidate
 					} else {
-						if (div > (forTest/2)) {
+						if (div > (forTest / 2)) {
 							pCount = pCount + 1;
 							div = 3; // resets div in prep for next candidate
 							break; // ends test for current candidate
@@ -66,11 +60,11 @@ public class FindNthPrime {
 						}
 					}
 				}
-				
 			}
-		System.out.println();
-		System.out.println ("Prime no. " + n + " = " + forTest);
-		System.out.println();			
+
+			System.out.println();
+			System.out.println("Prime no. " + n + " = " + forTest);
+			System.out.println();
 		}
 	}
 }
